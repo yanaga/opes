89234f66eef96caac4e82c4f33de0e6ae4701432
Ricardo Bespalhuk <rbespalhuk@gmail.com>
2016-09-26 22:18:35 -0300

    Fixed CertificadoDigital static factory method to store the password parameter instead of always the DEFAULT_CERTIFICATE_PASSWORD

--- src/main/java/me/yanaga/opes/CertificadoDigital.java
+++ src/main/java/me/yanaga/opes/CertificadoDigital.java
@@ -9,9 +9,9 @@ package me.yanaga.opes;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -135,7 +135,7 @@ public final class CertificadoDigital implements Serializable {
 				Instant expiry = ((X509Certificate) certs[0]).getNotAfter().toInstant();
 
 				ByteArrayOutputStream baos = new ByteArrayOutputStream();
-				keyStore.store(baos, DEFAULT_CERTIFICATE_PASSWORD);
+				keyStore.store(baos, password);
 
 				return new CertificadoDigital(baos.toByteArray(), extractCnpj(certs), expiry, (PrivateKey) key, (X509Certificate[]) certs);
 			}
